593540719270a0a8120a8dd3e49740459bc645b5
Noe Charmet <noe@rolo.chat>
2023-09-01 08:40:37 +0200

    Remove storage of key pair

--- client/src/vpn/components/VpnInit/VpnInit.tsx
+++ client/src/vpn/components/VpnInit/VpnInit.tsx
@@ -1,26 +1,16 @@
 import {PropsWithChildren} from 'react';
 import * as React from 'react';
-import {useGenerateKeyPair, useGetKeyPair} from 'vpn/data';
+import {useGenerateKeyPair} from 'vpn/data';
 import {useVpn, useVpnDispatch} from 'vpn/state';
 
 export function VpnInit({children}: PropsWithChildren) {
   const {publicKey, privateKey} = useVpn();
   const dispatch = useVpnDispatch();
-  const {isSuccess} = useGetKeyPair({
+
+  useGenerateKeyPair({
     enabled: !publicKey || !privateKey,
     suspense: true,
     onSuccess: keyPair => {
-      if (keyPair) {
-        dispatch({type: 'keyPair', payload: keyPair});
-      }
-    },
-  });
-
-  useGenerateKeyPair({
-    enabled: isSuccess && (!publicKey || !privateKey),
-    suspense: true,
-    onSuccess: keyPair => {
-      console.log(keyPair);
       dispatch({type: 'keyPair', payload: keyPair});
     },
   });
